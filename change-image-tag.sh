2b546448c12021a329ee58d6e328fcee8ac5635f
Hahajong <gkwhdtn95051@gmail.com>
2023-03-08 17:42:43 +0900

    modify

--- change-image-tag.sh
+++ change-image-tag.sh
@@ -4,4 +4,6 @@ IMAGE_NAME=$2
 BUILD_NUMBER=$3
 sed -i "s/image:.*/image: $IMAGE_REGISTRY_ACCOUNT\/$IMAGE_NAME:$BUILD_NUMBER/g" deployment.yaml
 git add deployment.yaml
+git config --global user.email "gkwhdtn95051@gmail.com"
+git config --global user.name "Hahajongsoo"
 git commit -m "Jenkins Build Number - $BUILD_NUMBER"
